3de8841891cd783f9e90cd1951c5bd50447a3300
Чинчи Саны-Шири <Sany-ShiriCE@mer.ci.nsu.ru>
2023-05-29 11:32:38 +0700

    Добавьте файлы проекта.

---
+++ Kruskal'sAlgorithmGraph/Graph.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kruskal_sAlgorithmGraph
+{
+
+    public class Edge
+    {
+        public int Source { get; set; }
+        public int Destination { get; set; }
+        public int Weight { get; set; }
+    }
+
+    public class Graph
+    {
+        private List<Edge> edges;
+
+        public Graph(List<Edge> graphEdges)
+        {
+            edges = graphEdges;
+        }
+
+        public List<Edge> GetMinimumSpanningTree()
+        {
+            List<Edge> minimumSpanningTree = new List<Edge>();
+
+            edges.Sort((x, y) => x.Weight.CompareTo(y.Weight));
+
+            int[] parent = new int[edges.Count];
+            for (int i = 0; i < edges.Count; i++)
+            {
+                parent[i] = i;
+            }
+
+            foreach (Edge edge in edges)
+            {
+                int sourceRoot = Find(parent, edge.Source);
+                int destinationRoot = Find(parent, edge.Destination);
+
+                if (sourceRoot != destinationRoot)
+                {
+                    minimumSpanningTree.Add(edge);
+                    Union(parent, sourceRoot, destinationRoot);
+                }
+            }
+
+            return minimumSpanningTree;
+        }
+
+        private int Find(int[] parent, int vertex)
+        {
+            if (vertex != parent[vertex])
+            {
+                parent[vertex] = Find(parent, parent[vertex]);
+            }
+            return parent[vertex];
+        }
+
+        private void Union(int[] parent, int source, int destination)
+        {
+            int sourceRoot = Find(parent, source);
+            int destinationRoot = Find(parent, destination);
+            parent[sourceRoot] = destinationRoot;
+        }
+    }
+
+}
---
+++ Kruskal'sAlgorithmGraph/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Kruskal_sAlgorithmGraph
+{
+    public class Program // Графы, алгоритм Краскала
+    {
+        public static void Main()
+        {
+            List<Edge> graphEdges = new List<Edge>()
+        {
+            new Edge { Source = 1, Destination = 2, Weight = 3 },
+            new Edge { Source = 1, Destination = 4, Weight = 2 },
+            new Edge { Source = 2, Destination = 3, Weight = 5 },
+            new Edge { Source = 2, Destination = 5, Weight = 4 },
+            new Edge { Source = 3, Destination = 6, Weight = 1 },
+            new Edge { Source = 6, Destination = 7, Weight = 3 },
+            new Edge { Source = 7, Destination = 8, Weight = 2 },
+            new Edge { Source = 4, Destination = 8, Weight = 6 },
+            new Edge { Source = 5, Destination = 8, Weight = 2 },
+            // Добавьте остальные ребра графа здесь
+        };
+
+            Graph graph = new Graph(graphEdges);
+            List<Edge> minimumSpanningTree = graph.GetMinimumSpanningTree();
+
+            Console.WriteLine("Ребра, включенные в минимальное остовное дерево:");
+            foreach (Edge edge in minimumSpanningTree)
+            {
+                Console.WriteLine($"{edge.Source} -- {edge.Destination} (вес: {edge.Weight})");
+            }
+        }
+    }
+}
